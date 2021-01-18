cb7db62d42c058fcce067c28aad0ae4f59833448
Stavros <stavros@snowball.digital>
2021-01-18 11:26:55 +0200

    prod url

--- shared.js
+++ shared.js
@@ -22,18 +22,15 @@ async function graphQLFetcher(graphQLParams) {
       throw new Error('You must insert your token ID and Secret');
     }
 
-    const response = await fetch(
-      'https://pim-dev.crystallize.digital/graphql',
-      {
-        method: 'post',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-Crystallize-Access-Token-Id': TOKEN.id,
-          'X-Crystallize-Access-Token-Secret': TOKEN.secret,
-        },
-        body: JSON.stringify(graphQLParams),
+    const response = await fetch('https://pim.crystallize.com/graphql', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Crystallize-Access-Token-Id': TOKEN.id,
+        'X-Crystallize-Access-Token-Secret': TOKEN.secret,
       },
-    );
+      body: JSON.stringify(graphQLParams),
+    });
 
     const json = await response.json();
     console.log(json.data);
