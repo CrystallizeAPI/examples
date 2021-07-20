a763253daa394b96e14590c9fd5360c42114abcd
Håkon Krogh <hakonhk@gmail.com>
2021-07-20 16:32:52 +0200

    Images example

---
+++ component-images/index.ts
@@ -0,0 +1,84 @@
+import {
+  Bootstrapper,
+  EVENT_NAMES,
+  JsonSpec,
+} from '@crystallize/import-utilities';
+
+const tenantSpec: JsonSpec = {
+  languages: [
+    {
+      code: 'en',
+      name: 'English',
+      isDefault: true,
+    },
+  ],
+  shapes: [
+    {
+      identifier: 'example-images',
+      name: 'Example images',
+      type: 'document',
+      components: [
+        {
+          id: 'some-images',
+          name: 'Some images',
+          type: 'images',
+        },
+      ],
+    },
+  ],
+  items: [
+    {
+      name: 'Example item with images',
+      shape: 'example-images',
+      components: {
+        'some-images': [
+          {
+            src: 'https://media.crystallize.com/crystallize_marketing/20/11/24/4/headless-ecommerce-crystallize.png',
+            altText: 'Headless eCommerce',
+            caption: {
+              json: [
+                {
+                  type: 'paragraph',
+                  textContent:
+                    'Headless eCommerce allows developers to build tailor made eCommerce experiences in their favourite frontend framework.',
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
+  ],
+};
+
+async function go() {
+  const bootstrapper = new Bootstrapper();
+
+  /**
+   * Access tokens for the account used
+   * https://crystallize.com/learn/developer-guides/access-tokens
+   */
+  const ACCESS_TOKEN_ID = process.env.CRYSTALLIZE_ACCESS_TOKEN_ID;
+  const ACCESS_TOKEN_SECRET = process.env.CRYSTALLIZE_ACCESS_TOKEN_SECRET;
+
+  bootstrapper.setAccessToken(ACCESS_TOKEN_ID, ACCESS_TOKEN_SECRET);
+
+  bootstrapper.setTenantIdentifier('<your-tenant-identifier-here>');
+
+  // bootstrapper.on(EVENT_NAMES.STATUS_UPDATE, (status) => {
+  //   console.log(JSON.stringify(status, null, 1));
+  // });
+
+  bootstrapper.on(EVENT_NAMES.DONE, (status) => {
+    console.log(
+      `Bootstrapped "${bootstrapper.tenantIdentifier}" in ${status.duration}`,
+    );
+    process.exit(0);
+  });
+
+  bootstrapper.setSpec(tenantSpec);
+
+  bootstrapper.start();
+}
+
+go();
---
+++ component-images/readme.md
@@ -0,0 +1,11 @@
+# Component Images
+
+Create an item with an [images](https://crystallize.com/learn/concepts/pim/component/images) component
+
+- Creates a new shape, `example-images`
+- Creates a new item based on the shape, "Example item with images"
+
+## Usage
+
+1. Install dependencies
+2. `yarn start` or `npm run start`
