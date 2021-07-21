cb550fe7a533d133d9fb53a69c3a05f226bdcf4b
Håkon Krogh <hakonhk@gmail.com>
2021-07-21 09:36:24 +0200

    Component choice

---
+++ component-choice/index.ts
@@ -0,0 +1,86 @@
+import {
+  Bootstrapper,
+  EVENT_NAMES,
+  JsonSpec,
+} from '@crystallize/import-utilities';
+
+const tenantSpec: JsonSpec = {
+  languages: [
+    {
+      code: 'en',
+      name: 'English',
+      isDefault: true,
+    },
+  ],
+  shapes: [
+    {
+      identifier: 'example-choice',
+      name: 'Example choice',
+      type: 'document',
+      components: [
+        {
+          id: 'a-choice',
+          name: 'A choice',
+          type: 'componentChoice',
+          config: {
+            choices: [
+              {
+                id: 'simple-text',
+                name: 'Simple text',
+                type: 'singleLine',
+              },
+              {
+                id: 'rich-text',
+                name: 'Rich text',
+                type: 'richText',
+              },
+            ],
+          },
+        },
+      ],
+    },
+  ],
+  items: [
+    {
+      name: 'Example item with choice',
+      shape: 'example-choice',
+      components: {
+        'a-choice': {
+          'simple-text': 'I simply made a choice',
+        },
+      },
+    },
+  ],
+};
+
+async function go() {
+  const bootstrapper = new Bootstrapper();
+
+  /**
+   * Access tokens for the account used
+   * https://crystallize.com/learn/developer-guides/access-tokens
+   */
+  const ACCESS_TOKEN_ID = process.env.CRYSTALLIZE_ACCESS_TOKEN_ID;
+  const ACCESS_TOKEN_SECRET = process.env.CRYSTALLIZE_ACCESS_TOKEN_SECRET;
+
+  bootstrapper.setAccessToken(ACCESS_TOKEN_ID, ACCESS_TOKEN_SECRET);
+
+  bootstrapper.setTenantIdentifier('<your-tenant-identifier-here>');
+
+  // bootstrapper.on(EVENT_NAMES.STATUS_UPDATE, (status) => {
+  //   console.log(JSON.stringify(status, null, 1));
+  // });
+
+  bootstrapper.on(EVENT_NAMES.DONE, (status) => {
+    console.log(
+      `Bootstrapped "${bootstrapper.tenantIdentifier}" in ${status.duration}`,
+    );
+    process.exit(0);
+  });
+
+  bootstrapper.setSpec(tenantSpec);
+
+  bootstrapper.start();
+}
+
+go();
---
+++ component-choice/readme.md
@@ -0,0 +1,11 @@
+# Component Choice
+
+Create an item with a [choice](https://crystallize.com/learn/concepts/pim/structural-component/choice) component
+
+- Creates a new shape, `example-choice`
+- Creates a new item based on the shape, "Example item with choice"
+
+## Usage
+
+1. Install dependencies
+2. `yarn start` or `npm run start`
