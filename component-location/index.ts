954efa1aa824e04c36a5595928d3b568ed8203c3
Håkon Krogh <hakonhk@gmail.com>
2021-07-21 15:02:46 +0200

    Location

---
+++ component-location/index.ts
@@ -0,0 +1,73 @@
+import {
+  Bootstrapper,
+  EVENT_NAMES,
+  JsonSpec,
+} from '@crystallize/import-utilities';
+
+const tenantSpec: JsonSpec = {
+  languages: [
+    {
+      code: 'en',
+      name: 'English',
+      isDefault: true,
+    },
+  ],
+  shapes: [
+    {
+      identifier: 'example-location',
+      name: 'Example location',
+      type: 'document',
+      components: [
+        {
+          id: 'a-location',
+          name: 'A location',
+          type: 'location',
+        },
+      ],
+    },
+  ],
+  items: [
+    {
+      name: 'Example item with location',
+      shape: 'example-location',
+      components: {
+        'a-location': {
+          lat: 74.006,
+          long: 40.7128,
+        },
+      },
+    },
+  ],
+};
+
+async function go() {
+  const bootstrapper = new Bootstrapper();
+
+  /**
+   * Access tokens for the account used
+   * https://crystallize.com/learn/developer-guides/access-tokens
+   */
+  const ACCESS_TOKEN_ID = process.env.CRYSTALLIZE_ACCESS_TOKEN_ID;
+  const ACCESS_TOKEN_SECRET = process.env.CRYSTALLIZE_ACCESS_TOKEN_SECRET;
+
+  bootstrapper.setAccessToken(ACCESS_TOKEN_ID, ACCESS_TOKEN_SECRET);
+
+  bootstrapper.setTenantIdentifier('<your-tenant-identifier-here>');
+
+  // bootstrapper.on(EVENT_NAMES.STATUS_UPDATE, (status) => {
+  //   console.log(JSON.stringify(status, null, 1));
+  // });
+
+  bootstrapper.on(EVENT_NAMES.DONE, (status) => {
+    console.log(
+      `Bootstrapped "${bootstrapper.tenantIdentifier}" in ${status.duration}`,
+    );
+    process.exit(0);
+  });
+
+  bootstrapper.setSpec(tenantSpec);
+
+  bootstrapper.start();
+}
+
+go();
---
+++ component-location/readme.md
@@ -0,0 +1,11 @@
+# Component Location
+
+Create an item with a [location](https://crystallize.com/learn/concepts/pim/component/location) component
+
+- Creates a new shape, `example-location`
+- Creates a new item based on the shape, "Example item with location"
+
+## Usage
+
+1. Install dependencies
+2. `yarn start` or `npm run start`
