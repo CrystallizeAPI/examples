f1f124f59785f07bf55ef09820a6656da34ceb44
Håkon Krogh <hakonhk@gmail.com>
2021-07-21 11:10:56 +0200

    Item relations

---
+++ component-item-relation/index.ts
@@ -0,0 +1,80 @@
+import {
+  Bootstrapper,
+  EVENT_NAMES,
+  JsonSpec,
+} from '@crystallize/import-utilities';
+
+const tenantSpec: JsonSpec = {
+  languages: [
+    {
+      code: 'en',
+      name: 'English',
+      isDefault: true,
+    },
+  ],
+  shapes: [
+    {
+      identifier: 'example-item-relation',
+      name: 'Example item relation',
+      type: 'document',
+      components: [
+        {
+          id: 'an-item-relation',
+          name: 'An item relation',
+          type: 'itemRelations',
+        },
+      ],
+    },
+  ],
+  items: [
+    {
+      name: 'Example item with item relation',
+      shape: 'example-item-relation',
+      components: {
+        'an-item-relation': [
+          {
+            externalReference: '12345678',
+          },
+        ],
+      },
+    },
+    {
+      name: 'Example item with external reference',
+      shape: 'example-item-relation',
+      externalReference: '12345678',
+    },
+  ],
+};
+
+async function go() {
+  const bootstrapper = new Bootstrapper();
+
+  /**
+   * Access tokens for the account used
+   * https://crystallize.com/learn/developer-guides/access-tokens
+   */
+  const ACCESS_TOKEN_ID = process.env.CRYSTALLIZE_ACCESS_TOKEN_ID;
+  const ACCESS_TOKEN_SECRET = process.env.CRYSTALLIZE_ACCESS_TOKEN_SECRET;
+
+  bootstrapper.setAccessToken(ACCESS_TOKEN_ID, ACCESS_TOKEN_SECRET);
+
+  // bootstrapper.setTenantIdentifier('<your-tenant-identifier-here>');
+  bootstrapper.setTenantIdentifier('hkn-examples');
+
+  // bootstrapper.on(EVENT_NAMES.STATUS_UPDATE, (status) => {
+  //   console.log(JSON.stringify(status, null, 1));
+  // });
+
+  bootstrapper.on(EVENT_NAMES.DONE, (status) => {
+    console.log(
+      `Bootstrapped "${bootstrapper.tenantIdentifier}" in ${status.duration}`,
+    );
+    process.exit(0);
+  });
+
+  bootstrapper.setSpec(tenantSpec);
+
+  bootstrapper.start();
+}
+
+go();
---
+++ component-item-relation/readme.md
@@ -0,0 +1,11 @@
+# Component Item Relation
+
+Create an item with an [item relation](https://crystallize.com/learn/concepts/pim/component/semantic-relation) component
+
+- Creates a new shape, `example-item-relation`
+- Creates a new item based on the shape, "Example item with item relation"
+
+## Usage
+
+1. Install dependencies
+2. `yarn start` or `npm run start`
