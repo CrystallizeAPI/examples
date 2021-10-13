a7305def544167eed48dc08ddc828a0679b26361
Håkon Krogh <hakonhk@gmail.com>
2021-10-13 12:51:35 +0200

    Add backup tenant example

---
+++ backup-tenant/README.md
@@ -0,0 +1,11 @@
+## Duplicate one tenant into another tenant
+
+This script creates a JSON spec for a Crystallize [PIM](https://crystallize.com/product/product-information-management) tenant, storing it on disk as a JSON backup. This backup
+file can later be used to [bootstrap a different tenant](https://github.com/CrystallizeAPI/examples/tree/main/duplicate-tenant).
+
+You will need an access token, in order to be able to run this script. It can be obtained under settings menu on [Access Tokens](https://pim.crystallize.com/settings/access-tokens)
+
+## Usage
+
+1. Install dependencies
+2. `yarn start` or `npm run start`
---
+++ backup-tenant/index.ts
@@ -0,0 +1,40 @@
+import fs from "fs/promises";
+import { Bootstrapper } from "@crystallize/import-utilities";
+
+async function backupTenant() {
+  const bootstrapper = new Bootstrapper();
+
+  /**
+   * Access tokens for the account used
+   * https://crystallize.com/learn/developer-guides/access-tokens
+   */
+  const ACCESS_TOKEN_ID = process.env.CRYSTALLIZE_ACCESS_TOKEN_ID;
+  const ACCESS_TOKEN_SECRET = process.env.CRYSTALLIZE_ACCESS_TOKEN_SECRET;
+
+  bootstrapper.setAccessToken(ACCESS_TOKEN_ID, ACCESS_TOKEN_SECRET);
+
+  bootstrapper.setTenantIdentifier("<tenant-identifier>");
+
+  const spec = await bootstrapper.createSpec({
+    grids: true,
+    items: true,
+    languages: true,
+    priceVariants: true,
+    shapes: true,
+    topicMaps: true,
+    vatTypes: true,
+    onUpdate: (areaUpdate) => {
+      console.log(areaUpdate.message);
+    },
+  });
+
+  await fs.writeFile(
+    `${__dirname}/backup-${bootstrapper.tenantIdentifier}-${Date.now()}.json`,
+    JSON.stringify(spec, null, 1),
+    "utf-8"
+  );
+
+  process.exit(0);
+}
+
+backupTenant();
