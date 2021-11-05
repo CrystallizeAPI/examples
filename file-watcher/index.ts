8c7ec6c3e342d37df8e41eadb46451ed141cc902
Håkon Krogh <hakonhk@gmail.com>
2021-11-05 13:25:39 +0100

    Updated file watcher example

--- file-watcher/index.ts
+++ file-watcher/index.ts
@@ -2,6 +2,10 @@ import fs from "fs/promises";
 import chokidar from "chokidar";
 
 import { Bootstrapper, EVENT_NAMES } from "@crystallize/import-utilities";
+import {
+  AreaUpdate,
+  AreaWarning,
+} from "@crystallize/import-utilities/dist/bootstrap-tenant/bootstrapper";
 
 /**
  * Access tokens for the account used
@@ -42,23 +46,50 @@ const worker = (function () {
 function runSpec(specPath: string): Promise<void> {
   return new Promise(async (resolve) => {
     const specString = await fs.readFile(specPath, "utf-8");
-
     const spec = JSON.parse(specString);
 
     const bootstrapper = new Bootstrapper();
+    const warnings: AreaWarning[] = [];
 
     bootstrapper.setAccessToken(ACCESS_TOKEN_ID, ACCESS_TOKEN_SECRET);
 
-    bootstrapper.setTenantIdentifier("<your-tenant-identifier-here>");
+    bootstrapper.setTenantIdentifier("hkn-examples");
 
-    // bootstrapper.on(EVENT_NAMES.STATUS_UPDATE, (status) => {
-    //   console.log(JSON.stringify(status, null, 1));
-    // });
+    bootstrapper.on(EVENT_NAMES.STATUS_UPDATE, (status: AreaUpdate) => {
+      // console.log(JSON.stringify(status, null, 1));
+      if (status.warning) {
+        warnings.push(status.warning);
+      }
+    });
 
     console.log(`"${specPath}": starting...`);
 
-    bootstrapper.on(EVENT_NAMES.DONE, (status) => {
-      console.log(`"${specPath}": finished in ${status.duration}`);
+    bootstrapper.on(EVENT_NAMES.DONE, async (status) => {
+      console.log(
+        `"${specPath}": finished in ${status.duration} with ${warnings.length} warnings`
+      );
+
+      /**
+       * Create a log entry if there are warnings
+       */
+      if (warnings.length > 1) {
+        const folderName = `${__dirname}/logs/log-${Date.now()}`;
+        await fs.mkdir(folderName);
+        await fs.writeFile(
+          `${folderName}/spec.json`,
+          JSON.stringify(spec, null, 1),
+          "utf-8"
+        );
+        await fs.writeFile(
+          `${folderName}/warnings.json`,
+          JSON.stringify(warnings, null, 1),
+          "utf-8"
+        );
+      }
+
+      // Optionally delete the spec file after it is done
+      // await fs.unlink(specPath);
+
       resolve();
     });
 
@@ -69,17 +100,17 @@ function runSpec(specPath: string): Promise<void> {
 }
 
 async function runAllSpecsInSequence() {
-  await runSpec("specs/run-me-first.json");
-  await runSpec("specs/run-me-next.json");
+  await runSpec(__dirname + "/specs/run-me-first.json");
+  await runSpec(__dirname + "/specs/run-me-next.json");
 }
 
 function watchDirectory() {
-  chokidar.watch("./specs/**/*.json").on("change", (path) => {
+  chokidar.watch(__dirname + "/specs/**/*.json").on("change", (path) => {
     console.log("Detected change to json file", path);
     worker.add(path);
   });
   chokidar
-    .watch("./specs/**/*.json", {
+    .watch(__dirname + "/specs/**/*.json", {
       ignoreInitial: true,
     })
     .on("add", (path) => {
