844bdae77e9f2b804c09e3c9ec8a91dc2ca6bf2a
Håkon Krogh <hakonhk@gmail.com>
2022-01-11 14:32:29 +0100

    No default tenant

--- multilingual-product/index.ts
+++ multilingual-product/index.ts
@@ -158,8 +158,7 @@ async function go() {
 
   bootstrapper.setAccessToken(ACCESS_TOKEN_ID, ACCESS_TOKEN_SECRET);
 
-  // bootstrapper.setTenantIdentifier("<your-tenant-identifier-here>");
-  bootstrapper.setTenantIdentifier("hkn-examples");
+  bootstrapper.setTenantIdentifier("<your-tenant-identifier-here>");
 
   // bootstrapper.on(EVENT_NAMES.STATUS_UPDATE, (status) => {
   //   console.log(JSON.stringify(status, null, 1));
