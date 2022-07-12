9c80993fe3a8ed1b06eb2b2bd8212a885eb79c57
ajami-rf <ajami@rightfarm.com>
2022-07-12 16:25:59 +0400

    Fixed env variables compilation issue. Added sample env file. (to be renamed to .env)

--- duplicate-tenant/index.ts
+++ duplicate-tenant/index.ts
@@ -1,3 +1,4 @@
+require('dotenv').config()
 import {
   Bootstrapper,
   EVENT_NAMES,
@@ -11,12 +12,12 @@ async function createSpec() {
    * Access tokens for the account used
    * https://crystallize.com/learn/developer-guides/access-tokens
    */
-  const ACCESS_TOKEN_ID = process.env.CRYSTALLIZE_ACCESS_TOKEN_ID;
-  const ACCESS_TOKEN_SECRET = process.env.CRYSTALLIZE_ACCESS_TOKEN_SECRET;
+  const ACCESS_TOKEN_ID = process.env.CRYSTALLIZE_ACCESS_TOKEN_ID ?? '';
+  const ACCESS_TOKEN_SECRET = process.env.CRYSTALLIZE_ACCESS_TOKEN_SECRET ?? '';
+  const SOURCE_TENANT_IDENTIFIER = process.env.SOURCE_TENANT_IDENTIFIER ?? '';
 
+  bootstrapper.setTenantIdentifier(SOURCE_TENANT_IDENTIFIER);
   bootstrapper.setAccessToken(ACCESS_TOKEN_ID, ACCESS_TOKEN_SECRET);
-
-  bootstrapper.setTenantIdentifier("<source-tenant-identifier>");
 
   return bootstrapper.createSpec();
 }
@@ -29,12 +30,12 @@ function importToTenant(spec: JsonSpec): Promise<void> {
      * Access tokens for the account used
      * https://crystallize.com/learn/developer-guides/access-tokens
      */
-    const ACCESS_TOKEN_ID = process.env.CRYSTALLIZE_ACCESS_TOKEN_ID;
-    const ACCESS_TOKEN_SECRET = process.env.CRYSTALLIZE_ACCESS_TOKEN_SECRET;
+    const ACCESS_TOKEN_ID = process.env.CRYSTALLIZE_ACCESS_TOKEN_ID ?? '';
+    const ACCESS_TOKEN_SECRET = process.env.CRYSTALLIZE_ACCESS_TOKEN_SECRET ?? '';
+    const TARGET_TENANT_IDENTIFIER = process.env.TARGET_TENANT_IDENTIFIER ?? '';
 
+    bootstrapper.setTenantIdentifier(TARGET_TENANT_IDENTIFIER);
     bootstrapper.setAccessToken(ACCESS_TOKEN_ID, ACCESS_TOKEN_SECRET);
-
-    bootstrapper.setTenantIdentifier("<target-tenant-identifier>");
 
     bootstrapper.on(EVENT_NAMES.STATUS_UPDATE, (status) => {
       console.log(JSON.stringify(status, null, 1));
